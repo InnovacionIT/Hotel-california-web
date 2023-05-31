50558a527a4678100a7a71d299a4973417ab7384
Natalia Moroni <nmoronidamasso@gmail.com>
2023-05-31 03:06:51 -0300

    other: se crean metodos get put delete y post para reservacion.service, se modifican las interfaces de reserva

--- HotelCalifornia2023/src/app/pages/habitaciones-ecommerce/product-card/product-card.component.ts
+++ HotelCalifornia2023/src/app/pages/habitaciones-ecommerce/product-card/product-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //import { Reserva } from '../../../interface/reserva.interface';
-import { Reserva } from '../../../models/reserva';
+import { Habitacion } from '../../../models/habitacion'
 
 @Component({
   selector: 'app-product-card',
@@ -8,19 +8,38 @@ import { Reserva } from '../../../models/reserva';
   styleUrls: ['./product-card.component.css']
 })
 export class ProductCardComponent implements OnInit {
-  public misReservas:Array<Reserva>;
+  public misHabitaciones:Array<Habitacion>;
+  public startDate: string;
+  public leaveDate: string;
+  public selectedStartDate: string;
+  public selectedLeaveDate: string;
 
-  constructor() { 
-    this.misReservas =[
-      new Reserva (`Habitación Single o Doble`, `Equipados con microondas, pava eléctrica y heladera tipo frigobar, ideales para prepararse una merienda o un snack (no aptos para comidas más elaboradas). TV-LED. Caja de seguridad en las habitaciones. Conexión a Internet inalámbrica (Wi-Fi).`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 3500),
-      new Reserva (`Habitación Triple`, `Equipados con cómodos Kitchenette de 2 x 1,8 m. aprox. con microondas, pava eléctrica y heladera tipo frigobar, ideales para prepararse una merienda o un snack (no aptos para comidas más elaboradas). TV-LED. Caja de seguridad en las habitaciones. Conexión a Internet inalámbrica (Wi-Fi). Sommiers de una plaza como camas adicionales.`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 3200),
-      new Reserva (`Habitación Cuádruple`, `Habitaciones familiares con 2 ambientes, dos camas doble plaza y 1 baño. Algunas con vistas al lago, TV 42 pulgadas con canales internacionales, minibar, etc.`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 3000),
-      new Reserva (`Suite Exclusiva`, `Amplia y elegante habitación equipada con una cama King, muebles de madera oscura, obras de arte en las paredes, baño privado con artículos de aseo gratuitos, escritorio, frigobar, cafetera,   caja fuerte,  sofá cama y sillón de descanso. Esta habitación cuenta con un balcón privado con sillones y  un gran  jacuzzi   que le garantizan un ambiente ideal para un placentero descanso disfrutando la naturaleza circundante`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 5500),
-      new Reserva (`Suite Junior Doble`, `Amplia habitación  equipada con dos camas queen. Disponen de muebles de madera oscura, obras de arte en las paredes, baño privado con artículos de aseo gratuitos, escritorio, frigobar,  caja fuerte, cafetera  terraza con sillones para disfrutar de la naturaleza con vista al jardín.`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 5000)
-    ]
+
+  constructor() {
+    this.misHabitaciones =[
+      new Habitacion (1, `Habitación Single o Doble`, `Equipados con microondas, pava eléctrica y heladera tipo frigobar, ideales para prepararse una merienda o un snack (no aptos para comidas más elaboradas). TV-LED. Caja de seguridad en las habitaciones. Conexión a Internet inalámbrica (Wi-Fi).`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 3500),
+      new Habitacion (2, `Habitación Triple`, `Equipados con cómodos Kitchenette de 2 x 1,8 m. aprox. con microondas, pava eléctrica y heladera tipo frigobar, ideales para prepararse una merienda o un snack (no aptos para comidas más elaboradas). TV-LED. Caja de seguridad en las habitaciones. Conexión a Internet inalámbrica (Wi-Fi). Sommiers de una plaza como camas adicionales.`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 3200),
+      new Habitacion (3, `Habitación Cuádruple`, `Habitaciones familiares con 2 ambientes, dos camas doble plaza y 1 baño. Algunas con vistas al lago, TV 42 pulgadas con canales internacionales, minibar, etc.`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 3000),
+      new Habitacion (4, `Suite Exclusiva`, `Amplia y elegante habitación equipada con una cama King, muebles de madera oscura, obras de arte en las paredes, baño privado con artículos de aseo gratuitos, escritorio, frigobar, cafetera,   caja fuerte,  sofá cama y sillón de descanso. Esta habitación cuenta con un balcón privado con sillones y  un gran  jacuzzi   que le garantizan un ambiente ideal para un placentero descanso disfrutando la naturaleza circundante`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 5500),
+      new Habitacion (5, `Suite Junior Doble`, `Amplia habitación  equipada con dos camas queen. Disponen de muebles de madera oscura, obras de arte en las paredes, baño privado con artículos de aseo gratuitos, escritorio, frigobar,  caja fuerte, cafetera  terraza con sillones para disfrutar de la naturaleza con vista al jardín.`, [`Baño privado`, `Tv LED`, `Microondas`, `Pava eléctrica`, `frigobar`, `Caja de Seguridad`, `Wi-fi`], true, 5000)
+    ];
+
+    const today = new Date(); // Obtener fecha actual
+    const sevenDaysLater = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000); // Sumar 7 días a la fecha actual
+
+    this.startDate = today.toISOString().split('T')[0]; // Convertir fecha a formato YYYY-MM-DD
+    this.leaveDate = sevenDaysLater.toISOString().split('T')[0]; // Convertir fecha a formato YYYY-MM-DD
+    this.selectedStartDate = this.startDate;
+    this.selectedLeaveDate = this.leaveDate;
+
+
   }
 
   ngOnInit(): void {
+  }
+
+  confirmReservation(habitacionId: number) {
+    // Lógica para confirmar la reserva utilizando el habitacionId y las fechas seleccionadas
   }
 
 }
--- HotelCalifornia2023/src/app/services/reserva.service.ts
+++ HotelCalifornia2023/src/app/services/reserva.service.ts
@@ -2,7 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Reserva } from 'src/app/models/reserva'; // pendiente de creacion
+import { Reserva } from 'src/app/models/habitacion'; // pendiente de creacion
 
 @Injectable({
   providedIn: 'root'
