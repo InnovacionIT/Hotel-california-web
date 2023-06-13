36d95b64db360f2b1c71936f122c9dc84e779bad
Fernando Tadeo <fernandotadeos@gmail.com>
2023-06-13 00:10:31 -0300

    FIX: Actualizacion de rama Develop

--- HotelCalifornia2023/src/app/app.module.ts
+++ HotelCalifornia2023/src/app/app.module.ts
@@ -8,11 +8,10 @@ import { SharedModule } from './shared/shared.module';
 import { PagesModule } from './pages/pages.module';
 
 
-
 @NgModule({
   declarations: [
     AppComponent,
-
+  
 
   ],
   imports: [
