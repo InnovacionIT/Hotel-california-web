2f22e8aeadef9a87e386f35945d87744d44b5616
cajalmarcelo <marcelo.cajal64@gmail.com>
2023-06-13 00:17:43 -0300

    FEATURE: CREACION DE TOKEN EN LOGIN

--- HotelCalifornia2023/BackendHotelCalifornia/hotel_auth/views.py
+++ HotelCalifornia2023/BackendHotelCalifornia/hotel_auth/views.py
@@ -5,26 +5,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .serializer import ClienteSerializer
+from rest_framework.authtoken.models import Token
 
 class LoginView(APIView):
-    def get(self, request):
-        return render(request, 'login.html')  # PROCESA SOLICITUD Y RENDERIZA LA VISTA "login.html"
-
     def post(self, request):  # METODO POST QUE OBTIENE EL USUARIO Y CONTRASEÑA DE LOS DATOS ENVIADOS EN LA SOLICITUD.
-        username = request.data.get('username')
+        usuario = request.data.get('usuario')
         password = request.data.get('password')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=usuario, password=password)
         if user is not None:
             login(request, user)
 
             if user.is_superuser:
-                return redirect('admin:index')  # Redirigir al panel de control de Django para el superusuario
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({'token de admin': token.key})  # Redirigir al panel de control de Django para el superusuario
             else:
-                return redirect('profile')  # Redirigir a la página de perfil para el usuario común
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({'token de usuario': token.key})
         else:
-            messages.error(request, 'Credenciales inválidas')
-            return redirect('login')
+            return Response({'error': 'Credenciales Invalidas'}, status=status.HTTP_401_UNAUTHORIZED)
 
 class LogoutView(APIView):
     def post(self, request):
