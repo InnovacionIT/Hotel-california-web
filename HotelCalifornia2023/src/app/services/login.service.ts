e158211d06d5a5207edbf1e94fea0b47f5872a9e
Federico Gonzalez <federicogonz2016@gmail.com>
2023-06-14 01:42:39 -0300

    REFACTOR: Login funcional, se soluciona error con CORS, se modifica archivo login.service.ts

--- HotelCalifornia2023/src/app/services/login.service.ts
+++ HotelCalifornia2023/src/app/services/login.service.ts
@@ -4,41 +4,34 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject, tap } from 'rxjs';
 import { User } from './user';
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  currentUserLoginOn : BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-  currentUserData : BehaviorSubject<User> = new BehaviorSubject<User>({clienteId:0, usuario:''});
+  currentUserLoginOn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  currentUserData: BehaviorSubject<User> = new BehaviorSubject<User>({ clienteId: 0, usuario: '' });
 
   constructor(private http: HttpClient) {}
 
-  login(_credentials:LoginRequest):Observable<User>{
-    return this.http.get<User>('http://localhost:8000/api/clientes/').pipe(
+  login(_credentials: LoginRequest): Observable<User> {
+    return this.http.post<User>('http://localhost:8000/api/auth/login/', _credentials).pipe(
       tap(userData => {
         this.currentUserData.next(userData);
         this.currentUserLoginOn.next(true);
       })
-    )
+    );
   }
 
-
-
-  get userData():Observable<User>{
-  return this.currentUserData.asObservable();
-
+  get userData(): Observable<User> {
+    return this.currentUserData.asObservable();
   }
- 
-  get userLoginOn():Observable<boolean>{
-  return this.currentUserLoginOn.asObservable();
 
+  get userLoginOn(): Observable<boolean> {
+    return this.currentUserLoginOn.asObservable();
   }
 
   logout(): void {
     this.currentUserData.next({ clienteId: 0, usuario: '' });
     this.currentUserLoginOn.next(false);
   }
-
-
 }
