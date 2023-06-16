cc80bee7eda4fe3c2555f8d53c1c275d21c5356b
Shirley L. FRASSA <sfrassa@ulasoft.com.ar>
2023-06-16 17:10:52 -0300

    Refactor: se modifica el tipo de vista para los endpoints y se refactoriza el codigo

--- HotelCalifornia2023/BackendHotelCalifornia/hotel_api/urls.py
+++ HotelCalifornia2023/BackendHotelCalifornia/hotel_api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import ReservaUpdateView, ReservaCreateView, ReservaDetailView, ReservaDeleteView
+from .views import ReservaUpdateView, ReservaCreateView, ReservaDetailView, ReservaDeleteView, ReservaView
+
+# urlpatterns = [
+#     path('reserva/', ReservaCreateView.as_view(), name='reserva'),
+#     path('reserva/<int:pk>/', ReservaUpdateView.as_view(), name='reserva-actualizar'),
+#     path('reserva/detalle/<int:pk>/', ReservaDetailView.as_view(), name='reserva-detalle'),
+#     path('reserva/delete/<int:pk>/', ReservaDeleteView.as_view(), name='reserva-delete'),
+# ]
 
 urlpatterns = [
-    path('reserva/', ReservaCreateView.as_view(), name='reserva'),
-    path('reserva/<int:pk>/', ReservaUpdateView.as_view(), name='reserva-actualizar'),
-    path('reserva/detalle/<int:pk>/', ReservaDetailView.as_view(), name='reserva-detalle'),
-    path('reserva/delete/<int:pk>/', ReservaDeleteView.as_view(), name='reserva-delete'),
+    path('reserva/', ReservaView.as_view(), name='reserva_list'),
+    path('reserva/<int:reservaId>/', ReservaView.as_view(), name='reserva_detail'),
 ]
--- HotelCalifornia2023/BackendHotelCalifornia/hotel_api/views.py
+++ HotelCalifornia2023/BackendHotelCalifornia/hotel_api/views.py
@@ -2,6 +2,66 @@ from rest_framework import status, generics
 from rest_framework.response import Response
 from .serializers import ReservaSerializer
 from GestionReservas.models import Reserva
+from rest_framework.views import APIView
+
+def validateDate(serializer):
+        fechaReserva = serializer.validated_data['fechaReserva']
+        fechaIngreso = serializer.validated_data['fechaIngreso']
+        fechaEgreso = serializer.validated_data['fechaEgreso']
+
+        if fechaEgreso <= fechaIngreso:
+            return Response({'error': 'Fecha de egreso debe ser posterior a la fecha de ingreso.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if fechaIngreso < fechaReserva:
+            return Response({'error': 'No se puede reservar una habitación para una fecha anterior a la fecha de reserva.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+def tryGetReservaById(reservaId):
+    try:
+        return Reserva.objects.get(pk=reservaId)
+    except Reserva.DoesNotExist:
+        return None
+
+class ReservaView(APIView):
+    def get(self, request, reservaId=None):
+        if reservaId is not None:  # Check if reservaId is provided
+            return self.get_by_id(request, reservaId)
+        reservas = Reserva.objects.all()
+        serializer = ReservaSerializer(reservas, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def get_by_id(self, request, reservaId):
+        reserva = tryGetReservaById(reservaId)
+        if reserva is None:
+            return Response({'error': 'Reserva not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ReservaSerializer(reserva)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = ReservaSerializer(data=request.data)
+        if serializer.is_valid():
+            validateDate(serializer)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, reservaId):
+        reserva = tryGetReservaById(reservaId)
+        if reserva is None:
+            return Response({'error': 'Reserva not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = ReservaSerializer(reserva, data=request.data)
+        if serializer.is_valid():
+            validateDate(serializer)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, reservaId):
+        reserva = tryGetReservaById(reservaId)
+        if reserva is None:
+            return Response({'error': 'Reserva not found'}, status=status.HTTP_404_NOT_FOUND)        
+        reserva.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class ReservaCreateView(generics.ListCreateAPIView):
     queryset = Reserva.objects.all()
@@ -10,16 +70,7 @@ class ReservaCreateView(generics.ListCreateAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            fechaReserva = serializer.validated_data['fechaReserva']
-            fechaIngreso = serializer.validated_data['fechaIngreso']
-            fechaEgreso = serializer.validated_data['fechaEgreso']
-
-            if fechaEgreso <= fechaIngreso:
-                return Response({'error': 'Fecha de egreso debe ser posterior a la fecha de ingreso.'}, status=status.HTTP_400_BAD_REQUEST)
-
-            if fechaIngreso < fechaReserva:
-                return Response({'error': 'No se puede reservar una habitación para una fecha anterior a la fecha de reserva.'}, status=status.HTTP_400_BAD_REQUEST)
-            
+            validateDate(serializer)
             self.perform_create(serializer)            
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
@@ -31,16 +82,7 @@ class ReservaUpdateView(generics.UpdateAPIView):
 
     def perform_update(self, serializer):
         if serializer.is_valid():
-            fechaReserva = serializer.validated_data['fechaReserva']
-            fechaIngreso = serializer.validated_data['fechaIngreso']
-            fechaEgreso = serializer.validated_data['fechaEgreso']
-
-            if fechaEgreso <= fechaIngreso:
-                return Response({'error': 'Fecha de egreso debe ser posterior a la fecha de ingreso.'}, status=status.HTTP_400_BAD_REQUEST)
-
-            if fechaIngreso < fechaReserva:
-                return Response({'error': 'No se puede reservar una habitación para una fecha anterior a la fecha de reserva.'}, status=status.HTTP_400_BAD_REQUEST)
-            
+            validateDate(serializer)
             serializer.save()            
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
